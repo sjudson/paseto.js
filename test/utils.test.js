fb990233a9b047bd857a8b3e08c2fbb56446798c
Samuel Judson <sam@sjudson.com>
2018-03-09 16:05:00 -0600

    More work on symmetric key v2.

--- test/utils.test.js
+++ test/utils.test.js
@@ -2,40 +2,40 @@ const assert = require('assert');
 
 describe('utilities', () => {
 
-  describe('encodead', () => {
+  describe('pae', () => {
 
-    const encodead = require('../lib/utils').encodead;
+    const pae = require('../lib/utils').pae;
 
     it('should encode no inputs', () => {
-      const encoded  = encodead();
+      const encoded  = pae();
       const expected = '0000000000000000';
 
       assert.equal(encoded.toString('hex'), expected);
     });
 
     it('should encode an empty string', () => {
-      const encoded  = encodead('');
+      const encoded  = pae('');
       const expected = '01000000000000000000000000000000';
 
       assert.equal(encoded.toString('hex'), expected);
     });
 
     it('should encode empty strings', () => {
-      const encoded  = encodead('', '');
+      const encoded  = pae('', '');
       const expected = '020000000000000000000000000000000000000000000000';
 
       assert.equal(encoded.toString('hex'), expected);
     });
 
     it('should encode a non-empty string', () => {
-      const encoded  = encodead('Paragon');
+      const encoded  = pae('Paragon');
       const expected = '0100000000000000070000000000000050617261676f6e';
 
       assert.equal(encoded.toString('hex'), expected);
     });
 
     it('should encode non-empty strings', () => {
-      const encoded  = encodead('Paragon', 'Initiative');
+      const encoded  = pae('Paragon', 'Initiative');
       const expected = '0200000000000000070000000000000050617261676f6e0a00000000000000496e6974696174697665';
 
       assert.equal(encoded.toString('hex'), expected);
@@ -45,7 +45,7 @@ describe('utilities', () => {
       const padding = Buffer.alloc(7).fill(0);
       const str     = 'Paragon' + String.fromCharCode(10) + padding + 'Initiative';
 
-      const encoded  = encodead(str);
+      const encoded  = pae(str);
       const expected = '0100000000000000190000000000000050617261676f6e0a00000000000000496e6974696174697665';
 
       assert.equal(encoded.toString('hex'), expected);
